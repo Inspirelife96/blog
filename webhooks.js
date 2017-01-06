aea8bc8d690419648d512f03fea21025d32f035c
Inspirelife96 <inspirelife@hotmail.com>
2017-01-06 16:15:07 +0800

    webhook

---
+++ webhooks.js
@@ -0,0 +1,9 @@
+var http = require('http')
+var exec = require('child_process').exec
+ 
+http.createServer(function (req, res) {
+if(req.url === '/webhooks/push/123456'){
+exec('sh ./deploy.sh')
+}
+res.end()
+}).listen(4002)
